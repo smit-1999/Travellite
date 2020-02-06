5a168243782765e44427951838e1156feaaaa5fc
smitshah1999@gmail.com <smitshah1999@gmail.com>
2020-02-06 22:40:24 +0530

    Corrected logout spelling

--- client/src/Components/Navigation_bar/Navigation_bar.js
+++ client/src/Components/Navigation_bar/Navigation_bar.js
@@ -1,70 +1,75 @@
-import React,{Component, useState} from 'react';
-import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
-import {Route} from 'react-router';
-import profile from '../Profile/Profile';
-import account from '../Account/Account';
-import login from '../Login/Login';
+import React, { Component, useState } from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import { Route } from "react-router";
+import profile from "../Profile/Profile";
+import account from "../Account/Account";
+import login from "../Login/Login";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
-    Container   
-  } from 'reactstrap';
-import {Dropdown} from 'react-bootstrap';
-import { DropdownDivider } from 'react-bootstrap/Dropdown';
-const bell = require('../../images/bell-png-5.png');
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+  Container
+} from "reactstrap";
+import { Dropdown } from "react-bootstrap";
+import { DropdownDivider } from "react-bootstrap/Dropdown";
+const bell = require("../../images/bell-png-5.png");
 
-  class Navigation_bar extends Component {
-
-    state={
-        isOpen : false
-    }
-    toggle=()=>{
+class Navigation_bar extends Component {
+  state = {
+    isOpen: false
+  };
+  toggle = () => {
     this.setState({
-        isOpen:!this.state.isOpen
+      isOpen: !this.state.isOpen
     });
-}
-render() {
-    return(
-        <div>
+  };
+  render() {
+    return (
+      <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
-            <Container>
-                <NavbarBrand href="/">
-                    Travellite
-                </NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                </Nav>
-                <img src={bell} width="30" height="30" className="d-inline-block align-top" 
-                alt="Notification bell" />
-                
-                <Dropdown >
-                    <Dropdown.Toggle color ="dark" variant="success" id="dropdown-basic">
-                    
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="/profile">Profile</Dropdown.Item>
-                        <Dropdown.Item href="/account">My Account</Dropdown.Item>
-                        <Dropdown.Divider></Dropdown.Divider>
-                        <Dropdown.Item href="/login">Log Out</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                </Collapse>
-            </Container>
+          <Container>
+            <NavbarBrand href="/">Travellit</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar></Nav>
+              <img
+                src={bell}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="Notification bell"
+              />
+
+              <pre class="tab"> </pre>
+
+              <Dropdown>
+                <Dropdown.Toggle
+                  color="dark"
+                  variant="success"
+                  id="dropdown-basic"
+                ></Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                  <Dropdown.Item href="/account">My Account</Dropdown.Item>
+                  <Dropdown.Divider></Dropdown.Divider>
+                  <Dropdown.Item href="/login">Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Collapse>
+          </Container>
         </Navbar>
-    </div>
-    );       
+      </div>
+    );
+  }
 }
-}
-export default Navigation_bar 
+export default Navigation_bar;
