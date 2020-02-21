c6d575587eb3ae77b9c4e2b1b85621a9f0272451
coderdhruv <f20170108@hyderabad.bits-pilani.ac.in>
2020-02-21 23:42:01 +0530

    UserRegistration and Login Form

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import './Login.css';
 import axios from 'axios';
+import { Button } from 'reactstrap';
+import { Link } from "react-router-dom";
+import { Route } from "react-router";
+import profile from "../Profile/Profile";
+import account from "../Account/Account";
+import login from "../Login/Login";
+import { Form } from 'reactstrap'
 
-class Login extends Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
-
-  // componentWillMount(){}
-  // componentDidMount(){}
-  // componentWillUnmount(){}
-
-  // componentWillReceiveProps(){}
-  // shouldComponentUpdate(){}
-  // componentWillUpdate(){}
-  // componentDidUpdate(){}
-  
-  handleClick(){
+class Login extends Component {  
+    constructor(){
+      super();
+      this.state = {
+        username : null,
+        password : null        
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleClick = this.handleClick.bind(this);
+      this.handleForgot = this.handleForgot.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {     
+      this.setState({username: event.target.value});      
+    }
+    handleClick(){
     console.log('Click happened');
    const data = {
       username: "zxc",
@@ -31,12 +38,66 @@ class Login extends Component {
       console.log('password incorrect');
     });
   }
+    handleForgot(){
+      alert("Credentials have been sent to your email address");
+    }  
+    handleSubmit() {
+       alert('A user was submitted: ' + this.state.password + this.state.username);      
+    }  
+    // mystyle = {
+    //   float : "middle",
+    //   height: "300px",
+    //   width: "500px",
+    //   padding: "10px",
+    //   fontFamily: "Arial",
+    //   border: "1px solid",
+    //   background: "009900",
+    //   margin : "200px"
+    // };
+    // constructor(props){
+    // super(props);
+    // this.state = {};
+    // }
+    // componentWillMount(){}
+    // componentDidMount(){}
+    // componentWillUnmount(){}
+
+    // componentWillReceiveProps(){}
+    // shouldComponentUpdate(){}
+    // componentWillUpdate(){}
+    // componentDidUpdate(){}
+
   render() {
     return (
+      <div class = "flex-container">           
+      <form>      
+        <br/>
+       <div title = "Username">        
+        <label>
+          Username :
+          <input type = "text"
+          placeholder = "username" 
+          value = {this.state.username} 
+          onChange = {e => this.setState({username : e.target.value})}
+           />
+        </label>
+      </div>
+        <br/>
+        <div title = "Password">
+        <label>
+          Password :
+          <input type = "text" 
+          placeholder = "password" 
+          value = {this.state.password} 
+          onChange = {e => this.setState({password : e.target.value})} />
+        </label>
+      </div>
       <div>
-        <button onClick={() => this.handleClick()}>
-          Login
-        </button>
+        <Button onClick = {this.handleSubmit} class = "submit"> Submit </Button>        
+        <br/>       
+        <Button type="button" class="forgotpsw" onClick = {this.handleForgot} class = "forgot">Forgot password</Button>    
+      </div>
+      </form>
       </div>
     );
   }
--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import './Register.css';
 import axios from 'axios';
+import { Button } from 'reactstrap';
+import { Link } from "react-router-dom";
+import { Route } from "react-router";
+import profile from "../Profile/Profile";
+import account from "../Account/Account";
+import { Form } from 'reactstrap'
 
 class Register extends Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
-
+  constructor(){
+    super();
+    this.state = {
+      firstname : null,
+      lastname : null,
+      email : null,
+      mob : null,
+      username : null,
+      password : null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleForgot = this.handleForgot.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleForgot.bind(this);
+  }  
   // componentWillMount(){}
   // componentDidMount(){}
   // componentWillUnmount(){}
@@ -16,7 +33,29 @@ class Register extends Component {
   // shouldComponentUpdate(){}
   // componentWillUpdate(){}
   // componentDidUpdate(){}
-  
+  handleChange(event) {     
+    this.setState({username: event.target.value});      
+  }
+  change(e){      
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleClick(){    
+    console.log(this.state);  
+    alert("Your credentials are submitted");
+  }
+  handleForgot(){
+    alert("Credentials have been sent to your email address");
+  }  
+  handleSubmit() {
+     alert('A user was submitted: ' + this.state.firstname + this.state.lastname + this.state.mob);      
+  }
+  handleReset(e) {
+    alert('A user was resetted: ');
+    e.preventDefault();
+    e.target.reset();
+ }
   handleClick(){
     console.log('Click happened');
    const data = {
@@ -37,11 +76,81 @@ class Register extends Component {
   }
   render() {
     return (
-      <div>
-        <button onClick={() => this.handleClick()}>
-          Register
-        </button>
+      <div class = "flex-container">           
+      <form>      
+        <br/>
+       <div title = "Firstname">        
+        <label>
+          Firstname :
+          <input type = "text"
+          placeholder = "firstname" 
+          value = {this.state.firstname} 
+          onChange = {e => this.setState({firstname : e.target.value})}
+           />
+        </label>
       </div>
+        <br/>
+        <div title = "lastname">
+        <label>
+          lastname :
+          <input type = "text" 
+          placeholder = "Lastname" 
+          value = {this.state.lastname} 
+          onChange = {e => this.setState({lastname : e.target.value})} />
+        </label>
+      </div>
+      <br/>
+        <div title = "email">
+        <label>
+          email :
+          <input type = "text" 
+          placeholder = "email" 
+          value = {this.state.email} 
+          onChange = {e => this.setState({email : e.target.value})} />
+        </label>
+      </div>
+      <br/>
+        <div title = "mob">
+        <label>
+          mob :
+          <input type = "text" 
+          placeholder = "mob" 
+          value = {this.state.mob} 
+          onChange = {e => this.setState({mob : e.target.value})} />
+        </label>
+      </div>
+      <br/>
+        <div title = "username">
+        <label>
+          username :
+          <input type = "text" 
+          placeholder = "username" 
+          value = {this.state.username} 
+          onChange = {e => this.setState({username : e.target.value})} />
+        </label>
+      </div>
+      <br/>
+        <div title = "password">
+        <label>
+          password :
+          <input type = "text" 
+          placeholder = "password" 
+          value = {this.state.password} 
+          onChange = {e => this.setState({password : e.target.value})} />
+        </label>
+      </div>      
+      <div>
+        <Button onClick = {this.handleSubmit} class = "submit"> Submit </Button>        
+        <br/>   
+      </div>
+      
+      </form>
+      </div>
+      // <div>
+      //   <button onClick={() => this.handleClick()}>
+      //     Register
+      //   </button>
+      // </div>
     );
   }
 }
