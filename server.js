5e6a22fd3d3ddd5f2548e419827e179ea150c96d
Smit <smitshah1999@gmail.com>
2020-02-23 22:20:51 +0530

    Version 1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const user = require("./routes/user.controller");
 const post = require("./routes/post.controller");
-
+const path = require("path");
 const app = express();
 app.use(cors());
 // app.use(cors({origin: true, credentials: true}));
@@ -29,6 +29,13 @@ connection.once("open", () => {
 //use routes
 app.use("/user", user);
 app.use("/post", post);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT | 4000;
 
 app.listen(port, () => console.log("Server started on port " + port));
