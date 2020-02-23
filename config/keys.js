6574496278a05a88972b070dafbb76c7bd6fb478
Smit <smitshah1999@gmail.com>
2020-02-23 19:58:44 +0530

    changed base_uri

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://user:smitshah@cluster0-slqvc.mongodb.net/Travellite?retryWrites=true&w=majority",
-  base_uri: "http://localhost:4000"
+  base_uri: "https://travellite-bphc.herokuapp.com"
 };
