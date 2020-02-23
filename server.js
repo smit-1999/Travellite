bc07889013861dedb34eb4481f480ccc1b950bb4
Smit <smitshah1999@gmail.com>
2020-02-23 23:53:36 +0530

    changed app.use

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.use("/user", user);
 app.use("/post", post);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
   app.get("*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    res.redirect("https://google.com");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    //res.redirect("https://google.com");
   });
 }
 const port = process.env.PORT | 4000;
