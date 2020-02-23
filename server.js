7a6ad485dbb7dea37bb62349e3637cc44b7f55e6
Smit <smitshah1999@gmail.com>
2020-02-24 01:20:51 +0530

    final

--- server.js
+++ server.js
@@ -24,10 +24,12 @@ mongoose.connect(uri, {
   useUnifiedTopology: true
 });
 const connection = mongoose.connection;
+mongoose.Promise = global.Promise;
 connection.once("open", () => {
   console.log("Mongoose connected");
 });
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //use routes
 app.use("/user", user);
 app.use("/post", post);
@@ -46,16 +48,8 @@ app.use("/post", post);
 // });
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  //   //res.redirect("https://google.com");
-  // });
-  app.get("/", function(req, res) {
-    var todos = ["Hiii", "Byee", "Good night"];
-    console.log(todos);
-    console.log(req.user);
-    res.send({ todos: todos, user: req.user });
-    //res.render("index.ejs");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 const port = process.env.PORT | 4000;
