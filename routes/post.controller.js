7cd4fb36b2dbb44fea96c8fb4898595bd31c276b
Smit <smitshah1999@gmail.com>
2020-02-24 00:23:29 +0530

    sjdkgh

--- routes/post.controller.js
+++ routes/post.controller.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Post = require("../models/post.model");
 
 router.get("/", (req, res) => {
-  Post.find(req.query).then(posts => {
+  Post.findOne(req.query).then(posts => {
     console.log("request to router:", req.query);
     console.log("Resp to search query :", posts);
     return res.json(posts);
--- routes/user.controller.js
+++ routes/user.controller.js
@@ -1,73 +1,82 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
 
 //Item model
-const User = require('../models/user.model');
+const User = require("../models/user.model");
 
-router.get('/',(req, res) => {
-    User.find().then(users => res.json(users));
+router.get("/", (req, res) => {
+  User.findOne().then(users => res.json(users));
 });
 
-router.post('/add',(req, res) => {
-    addUser(req.body)
+router.post("/add", (req, res) => {
+  addUser(req.body)
     .then(res_object => res.json(res_object))
-    .catch(err => res.json(err));    
+    .catch(err => res.json(err));
 });
-async function addUser({firstname, lastname, email, mob, username , password}){
-    const user = await User.findOne({ username });
-    res_object = {
-        'status': '',
-        'message': '',
-    }
-    if (user) {
-        res_object['status'] = 'failure'
-        res_object['message'] = 'username already exists'
-        console.log(res_object);
-        return res_object         
-    }else{
-        User.create({firstname, lastname, email, mob, username , password}, 
-            function(err, instance) {
-        if(err) {
-            res_object['status'] = 'failure'
-            res_object['message'] = 'database error'
-            console.log(res_data['status'] + ' ' + res_object['message']);
-            console.log(err);
-            return res_object;
+async function addUser({
+  firstname,
+  lastname,
+  email,
+  mob,
+  username,
+  password
+}) {
+  const user = await User.findOne({ username });
+  res_object = {
+    status: "",
+    message: ""
+  };
+  if (user) {
+    res_object["status"] = "failure";
+    res_object["message"] = "username already exists";
+    console.log(res_object);
+    return res_object;
+  } else {
+    User.create(
+      { firstname, lastname, email, mob, username, password },
+      function(err, instance) {
+        if (err) {
+          res_object["status"] = "failure";
+          res_object["message"] = "database error";
+          console.log(res_data["status"] + " " + res_object["message"]);
+          console.log(err);
+          return res_object;
+        } else {
+          res_object["status"] = "success";
+          res_object["message"] = "User added to the database";
+          console.log(res_object);
+          return res_object;
         }
-        else{
-            res_object['status'] = 'success'
-            res_object['message'] = 'User added to the database';
-            console.log(res_object);
-            return res_object;                         
-        }
-    });
-}
+      }
+    );
+  }
 }
 
-router.post('/authentication', authentication);
+router.post("/authentication", authentication);
 
 function authentication(req, res, next) {
-    authenticate(req.body)
-        .then(user => user ? res.json(user) : res.json({ message: 'password is incorrect' }))
-        .catch(err => next(err));
+  authenticate(req.body)
+    .then(user =>
+      user ? res.json(user) : res.json({ message: "password is incorrect" })
+    )
+    .catch(err => next(err));
 }
 
 async function authenticate({ username, password }) {
-    const user = await User.findOne({ username });
-    if (!user) {
-        return { username: "Username not found" };
-      }
-    else if (user && !password.localeCompare(user.password)) {
-        return {
-            username,
-            message : "login succesful"
-        };
-    }else{
-        return{
-            username,
-            message:"password incorrect"
-        }
-    }
+  const user = await User.findOne({ username });
+  if (!user) {
+    return { username: "Username not found" };
+  } else if (user && !password.localeCompare(user.password)) {
+    return {
+      username,
+      message: "login succesful"
+    };
+  } else {
+    return {
+      username,
+      message: "password incorrect"
+    };
+  }
 }
 
 module.exports = router;
