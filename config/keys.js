218caf56a23d2163972703011c80f474a50e834e
Smit <smitshah1999@gmail.com>
2020-02-24 19:36:35 +0530

    check

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGO_URI:
     "mongodb+srv://user:smitshah@cluster0-slqvc.mongodb.net/Travellite?retryWrites=true&w=majority",
-  base_uri: "https://travellite-bphc.herokuapp.com"
+  base_uri: "https://tranquil-oasis-05127.herokuapp.com"
 };
