556f0fa5165f42114f0ea3f28441d42d3fd41e62
Saarthak Jain <f20170083@hyderabad.bits-pilani.ac.in>
2020-03-30 01:00:36 +0530

    Modified dateFormat with DD-MM-YYYY in addPost

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
             <Route path="/account" component={Account} />
             <Route path="/search" component={SearchPage} />
             <Route path="/about" component={AboutPage} />
-
+            <Route path="/addPost" component={AddPost} />
             <Route path="/">
               {" "}
               This is the index page.Select /x to go to page x
