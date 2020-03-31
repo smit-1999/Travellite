4bc7f0873eb6f375ee46a4cf511c0ed177c9aabd
shik-25 <shikhar.dhawale@gmail.com>
2020-04-01 00:54:01 +0530

    login necessary to browse other pages

--- client/src/Components/Navigation_bar/Navigation_bar.js
+++ client/src/Components/Navigation_bar/Navigation_bar.js
@@ -51,12 +51,7 @@ class Navigation_bar extends Component {
 
     this.toggleCollapse = this.toggleCollapse.bind(this);
     this.toggleLogin = this.toggleLogin.bind(this);
-
-    //this.handleChange = this.handleChange.bind(this);
-    //this.handleForgot = this.handleForgot.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
-    this.toggleLogin = this.toggleLogin.bind(this);
-  }
+}
   componentDidMount() {
     const user = localStorage.getItem("user");
     this.setState({
