d5f07205eedcb1bcd1814e9af27f688186da0788
shik-25 <shikhar.dhawale@gmail.com>
2020-04-01 01:10:30 +0530

    login necessary to browse other pages

--- client/src/Components/Navigation_bar/Navigation_bar.js
+++ client/src/Components/Navigation_bar/Navigation_bar.js
@@ -68,7 +68,6 @@ class Navigation_bar extends Component {
     localStorage.setItem("loggedIn", false);
     localStorage.setItem("user","");
     };
-
   toggleCollapse = () => {
     this.setState({
       isOpen: !this.state.isOpen
