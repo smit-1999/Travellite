a397474fbd4d340a6947561e625b1c3954e73f27
Saarthak Jain <f20170083@hyderabad.bits-pilani.ac.in>
2020-04-01 18:02:10 +0530

    Axios Post request to get notifications in frontend

--- client/src/Components/Navigation_bar/Navigation_bar.js
+++ client/src/Components/Navigation_bar/Navigation_bar.js
@@ -13,6 +13,9 @@ import {
   MDBDropdownItem,
   MDBIcon
 } from "mdbreact";
+
+import { Button, Jumbotron, ListGroup } from "react-bootstrap";
+import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // import ReactDOM from "react-dom";
@@ -39,32 +42,57 @@ import {
 import { Dropdown } from "react-bootstrap";
 import { DropdownDivider } from "react-bootstrap/Dropdown";
 const bell = require("../../images/bell-png-5.png");
+const base_url = require("../../config/keys").base_uri;
 
 class Navigation_bar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
-      isOpen: false
+      isOpen: false,
+      notifs: []
     };
     this.componentDidMount = this.componentDidMount.bind(this);
 
     this.toggleCollapse = this.toggleCollapse.bind(this);
+
+    this.getNotification = this.getNotification.bind(this);
+
     //this.handleChange = this.handleChange.bind(this);
     //this.handleForgot = this.handleForgot.bind(this);
     //this.handleSubmit = this.handleSubmit.bind(this);
     this.toggleLogin = this.toggleLogin.bind(this);
   }
+
+  getNotification = () => {
+    console.log("hyyde");
+    axios
+      .post(base_url + "/notif/ownerRequests", {
+        userName: "Saarthak"
+      })
+      .then(function(response) {
+        this.setState({ notifs: response.data });
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    console.log(this.state.notifs);
+  };
+
   componentDidMount() {
     const user = localStorage.getItem("user");
     this.setState({
       username: user
     });
+    console.log("heelo");
   }
+
   toggleLogin = () => {
     localStorage.setItem("loggedIn", false);
-    localStorage.setItem("user","");
-    };
+    localStorage.setItem("user", "");
+  };
+
   toggleCollapse = () => {
     this.setState({
       isOpen: !this.state.isOpen
@@ -72,44 +100,49 @@ class Navigation_bar extends Component {
   };
   render() {
     return (
-
-      <div>        
-      <MDBNavbar color="black" dark expand="md">
-        <MDBNavbarBrand href="/search">
-          <strong className="white-text" >Travellite</strong>
-        </MDBNavbarBrand>
-        <MDBNavbarToggler onClick={this.toggleCollapse} />
-         <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-          <MDBNavbarNav left>
-    <MDBNavItem><strong className="white-text">Hi {this.state.username}</strong></MDBNavItem>
-          </MDBNavbarNav>
-          <MDBNavbarNav right>
-          <MDBNavItem>
-            <MDBNavbarBrand>
-              <strong>
-          <i className="far fa-bell"></i></strong></MDBNavbarBrand>
-            </MDBNavItem>
-            <MDBNavItem>
-               <MDBDropdown>
-                <MDBDropdownToggle nav caret>
-                  <MDBIcon icon="user" />
-                </MDBDropdownToggle>
-                <MDBDropdownMenu className="dropdown-default">
-                   <MDBDropdownItem href="/profile">Profile</MDBDropdownItem>
-                  <MDBDropdownItem href="/account">Account</MDBDropdownItem>
-    <MDBDropdownItem href="/login" onClick={this.toggleLogin}>Logout</MDBDropdownItem>  
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
-            <MDBNavItem>
-            <MDBNavbarBrand href="/about"><strong className="white-text align-center" >
-              About Us
-              </strong></MDBNavbarBrand>
-            </MDBNavItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBNavbar>
-    </div>
+      <div>
+        <MDBNavbar color="black" dark expand="md">
+          <MDBNavbarBrand href="/search">
+            <strong className="white-text">Travellite</strong>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav left>
+              <MDBNavItem>
+                <strong className="white-text">Hi {this.state.username}</strong>
+              </MDBNavItem>
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+              <MDBNavItem>
+                <MDBNavbarBrand>
+                  <strong>
+                    <i className="far fa-bell"></i>
+                  </strong>
+                </MDBNavbarBrand>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle nav caret>
+                    <MDBIcon icon="user" />
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu className="dropdown-default">
+                    <MDBDropdownItem href="/profile">Profile</MDBDropdownItem>
+                    <MDBDropdownItem href="/account">Account</MDBDropdownItem>
+                    <MDBDropdownItem href="/login" onClick={this.toggleLogin}>
+                      Logout
+                    </MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavbarBrand href="/about">
+                  <strong className="white-text align-center">About Us</strong>
+                </MDBNavbarBrand>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBNavbar>
+      </div>
     );
   }
 }
