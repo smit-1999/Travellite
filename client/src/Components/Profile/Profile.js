5eefc7c4857af1321f58127f03666a34842e219e
Saarthak Jain <f20170083@hyderabad.bits-pilani.ac.in>
2020-04-01 20:07:58 +0530

    GET request for user details in profile section

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import './Profile.css';
-
+import "./Profile.css";
+import axios from "axios";
+import { Button } from "react-bootstrap";
+const base_url = require("../../config/keys").base_uri;
 class Profile extends Component {
-  // constructor(props){
-    // super(props);
-    // this.state = {};
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { userDetails: [], userName: "" };
+
+    this.getUserDetails = this.getUserDetails.bind(this);
+  }
 
   // componentWillMount(){}
   // componentDidMount(){}
@@ -17,18 +21,37 @@ class Profile extends Component {
   // componentWillUpdate(){}
   // componentDidUpdate(){}
 
+  getUserDetails = () => {
+    console.log("bhkm");
+    const params = {
+      userName: "Saarthak"
+    };
+    axios
+      .get(base_url + "/user/userDetails", { params })
+      .then(function(response) {
+        this.setState({ userDetails: response.data });
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    console.log(this.state.userDetails);
+  };
+
+  componentDidMount() {
+    const user = localStorage.getItem("user");
+    this.setState({
+      userName: user
+    });
+    console.log("heelo");
+  }
+
   render() {
     const loggedIn = localStorage.getItem("loggedIn");
-    if(loggedIn.localeCompare("false")===0){
-      return (
-        <Redirect
-          to="/Login"
-        />
-      );
+    if (loggedIn.localeCompare("false") === 0) {
+      return <Redirect to="/Login" />;
     }
-    return (
-      <div>Profile works</div>
-    );
+    return <div>Profile works</div>;
   }
 }
 
