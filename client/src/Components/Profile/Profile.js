5823c6cbf064aacae8c49ff8744750966486b03f
Smit <smitshah1999@gmail.com>
2020-04-04 00:19:13 +0530

    adds frontend for myprofile

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import "./Profile.css";
 import axios from "axios";
-import { Button } from "react-bootstrap";
+import {
+  MDBJumbotron,
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBCardBody,
+  MDBCardText,
+  MDBCardTitle,
+  MDBCardImage
+} from "mdbreact";
 const base_url = require("../../config/keys").base_uri;
 class Profile extends Component {
   constructor(props) {
@@ -47,11 +58,61 @@ class Profile extends Component {
   }
 
   render() {
+    let username = "smit",
+      mob = "123",
+      emailid = "x@g.com";
     const loggedIn = localStorage.getItem("loggedIn");
     if (loggedIn.localeCompare("false") === 0) {
       return <Redirect to="/Login" />;
+    } else {
+      //this.getUserDetails();
+      //console.log(this.state.userDetails);
     }
-    return <div>Profile works</div>;
+    return (
+      <div>
+        <MDBContainer className="mt-5 text-center">
+          <MDBRow>
+            <MDBCol>
+              <MDBJumbotron>
+                <MDBCardBody>
+                  <MDBCardImage
+                    className="img-fluid"
+                    src="https://cdn2.f-cdn.com/contestentries/1465388/8851154/5c3e839cde852_thumb900.jpg"
+                    waves
+                  />
+                  <MDBCardTitle className="h2">
+                    My Account Details
+                    <div>
+                      {" "}
+                      <MDBIcon icon="user-check" />{" "}
+                    </div>
+                  </MDBCardTitle>
+                  <hr className="my-4" />
+                  <MDBRow className="mt-3">
+                    {" "}
+                    Username <MDBIcon icon="user" className="pl-3" />
+                  </MDBRow>
+                  <MDBRow className="mt-3">
+                    {" "}
+                    Email Id <MDBIcon icon="envelope" className="pl-3" />{" "}
+                  </MDBRow>
+                  <MDBRow className="mt-3">
+                    {" "}
+                    Mobile <MDBIcon icon="mobile-alt" className="pl-3" />
+                  </MDBRow>
+
+                  <div className="pt-2">
+                    <MDBBtn outline color="primary" className="waves-effect">
+                      Update <MDBIcon far icon="edit" />
+                    </MDBBtn>
+                  </div>
+                </MDBCardBody>
+              </MDBJumbotron>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
+    );
   }
 }
 
