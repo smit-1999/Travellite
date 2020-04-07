a9629b0fe46988ee72cab813a0dda2afd9f59900
Smit <smitshah1999@gmail.com>
2020-04-07 17:59:10 +0530

    deploy before sprint2

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //base_uri: "https://tranquil-oasis-05127.herokuapp.com"
-  base_uri: "http://localhost:4000"
+  base_uri: "https://tranquil-oasis-05127.herokuapp.com",
+  //base_uri: "http://localhost:4000"
 };
