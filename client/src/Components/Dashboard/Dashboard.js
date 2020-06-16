d0526c8afae9fae7e2d2b8507bc1f730dcea0bc1
Smit <smitshah1999@gmail.com>
2020-06-16 20:55:31 +0530

    updates map => foreach

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -148,10 +148,10 @@ class Dashboard extends Component {
         });
         console.log(this.state.list1 + ":list1");
         const m = new Map();
-        array.map((num) => {
+        array.forEach((num) => {
           m.set(num, 0);
         });
-        list_temp2.map((num) => {
+        list_temp2.forEach((num) => {
           m.set(num, m.get(num) + 1);
         });
         const a1 = [...m.values()];
@@ -163,7 +163,7 @@ class Dashboard extends Component {
         });
         console.log(this.state.list1);
         console.log(this.state.list2);
-        array.map(() => {
+        array.forEach(() => {
           var color = randomColor();
           list_temp4.push(color);
         });
